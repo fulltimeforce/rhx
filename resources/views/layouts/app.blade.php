ad504d7b263c4217e99547f05443c966fcb61591
Edwin Soria <edwin.soria@fulltimeforce.com>
2020-03-06 15:48:05 -0500

    fixed text table experts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -100,7 +100,9 @@
         <script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script>
         <script type="text/javascript" src="{{ asset('/tablefilter/tablefilter.js') }}"></script>
-
+        <script>
+            var $_url_ajax = '{!! env("APP_URL_AJAX") !!}';
+        </script>
         @yield('javascript')
     </div>
 </body>
