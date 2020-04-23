ff34c3b75d8d1260d606c54c5d6d52e6475afdb1
Edwin Soria <edwin.soria@fulltimeforce.com>
2020-04-23 10:37:41 -0500

    change url portfolio

--- resources/views/portfolio/template.blade.php
+++ resources/views/portfolio/template.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-Frame-Options" content="deny">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <meta name="description" content="">
-  <meta name="author" content="">
+  >
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
   <title>{{ $expert->fullname }}</title>
   <link rel="stylesheet" href="{{ asset('/portfolio/css/color.css') }}?v={{ strtotime( date('Y-m-d H:i:s') ) }}">
   <link href="https://fonts.googleapis.com/css?family=Roboto|Roboto+Mono|Inconsolata" rel="stylesheet">
