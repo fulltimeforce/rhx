eaf0a815b55b49089af07fcca8ff0bd6aa859ffd
Edwin Soria <edwin.soria@fulltimeforce.com>
2020-05-19 11:17:51 -0500

    comment upload drive

--- app/Http/Controllers/ExpertController.php
+++ app/Http/Controllers/ExpertController.php
@@ -217,26 +217,26 @@ class ExpertController extends Controller
                 $_fileName = "cv-".date("Y-m-d")."-".time().".".$file->getClientOriginalExtension();
                 $newNameFile = $destinationPath."/" . $_fileName;
                 $input["file_path"] = $newNameFile;
-                $mimeType = $file->getMimeType();
+                // $mimeType = $file->getMimeType();
                 $file->move( $destinationPath, $newNameFile );
                 if( Auth::check() ){
                     // $name = gettype($file) === 'object' ? $file->getClientOriginalName() : $file;
-                    $fileMetadata = new \Google_Service_Drive_DriveFile([
-                        'name' => $_fileName,
-                        // 'parents' => array( env('GOOGLE_FOLDER_ID') )
-                    ]);
+                    // $fileMetadata = new \Google_Service_Drive_DriveFile([
+                    //     'name' => $_fileName,
+                    //     // 'parents' => array( env('GOOGLE_FOLDER_ID') )
+                    // ]);
 
                     // $content = gettype($file) === 'object' ?  File::get($file) : Storage::get($file);
                     // $mimeType = gettype($file) === 'object' ? File::mimeType($file) : Storage::mimeType($file);
 
-                    $content = file_get_contents( $newNameFile );
+                    // $content = file_get_contents( $newNameFile );
                     
-                    $_file = $this->drive->files->create($fileMetadata, [
-                        'data' => $content,
-                        'mimeType' => $mimeType,
-                        'uploadType' => 'multipart',
-                        'fields' => 'id'
-                    ]);
+                    // $_file = $this->drive->files->create($fileMetadata, [
+                    //     'data' => $content,
+                    //     'mimeType' => $mimeType,
+                    //     'uploadType' => 'multipart',
+                    //     'fields' => 'id'
+                    // ]);
                 }
             }
 
