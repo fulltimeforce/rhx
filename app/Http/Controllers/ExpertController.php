93cfe1929bf9aaf657562f8c4dd9eff800bb4b71
Edwin Soria <edwin.soria@fulltimeforce.com>
2020-05-19 12:05:40 -0500

    comment constructor expert

--- app/Http/Controllers/ExpertController.php
+++ app/Http/Controllers/ExpertController.php
@@ -31,17 +31,17 @@ class ExpertController extends Controller
     private $client;
     private $drive;
 
-    public function __construct(Google_Client $client)
-    {
-        if( Auth::check() ){
-            $this->middleware(function ($request, $next) use ($client) {
-                $client->refreshToken(Auth::user()->access_token);
-                $this->drive = new Google_Service_Drive($client);
-                return $next($request);
-            });
-        }
+    // public function __construct(Google_Client $client)
+    // {
+    //     if( Auth::check() ){
+    //         $this->middleware(function ($request, $next) use ($client) {
+    //             $client->refreshToken(Auth::user()->access_token);
+    //             $this->drive = new Google_Service_Drive($client);
+    //             return $next($request);
+    //         });
+    //     }
         
-    }
+    // }
 
     public function index( Request $request )
     {
