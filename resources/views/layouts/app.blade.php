b16a3d05af0006ea5f8e40e4dde179cc366406f9
Paul Fajardo <paulfajardo12356@gmail.com>
2020-10-07 10:50:54 -0500

    Hide RECRUITMENT

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -82,9 +82,9 @@
                                 <a class="nav-link" href="{{ route('expert.portfolio.resume') }}">{{ __('Resume') }}</a>
                             </li> -->
                             @if( Auth::user()->role->id < 3 )
-                            </li><li class="nav-item">
+                            <!--</li><li class="nav-item">
                                 <a class="nav-link" href="{{ route('recruit.menu') }}">{{ __('RECRUITMENT') }}</a>
-                            </li>
+                            </li>-->
                             </li><li class="nav-item">
                                 <a class="nav-link" href="{{ route('recruiter.log') }}">{{ __('Logs') }}</a>
                             </li>
